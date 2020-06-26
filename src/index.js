21109548b32f15490922ebbc22161e1b0c6abd4b
Jonathan <jonathandias96@gmail.com>
2020-06-25 23:15:47 -0300

    created home page without components of select

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
+
+import Home from './pages/Home';
 
 export default function App(){
-    return <View/>
+    return(
+        <>
+            <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent/>
+            <Home/>
+        </>
+    ); 
 }
+
